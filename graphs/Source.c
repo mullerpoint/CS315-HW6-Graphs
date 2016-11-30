d2906168bd80d0e39f552f67188685f5140656d1
mullerpoint <gary@mullerpoint.com>
2016-11-29 18:09:26 -0700

    Boilerplate add

---
+++ graphs/Source.c
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Gary Muller
+// Fall 2016
+// CS 315 Assignment 6
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// A program that creates and utilizes a graph structure, it will utilize a
+// struct type def, and includes an add functionality; it will utilize a
+// stack to allow for traversals
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Header files standard libraries and classes
+#include <stdio.h> //standard io functions for C
+#include <stdlib.h> //used for better malloc() function
+#include <ctype.h> //used for tolower() function
+#include <stdbool.h> //used for bool types
+#include <string.h> //string functions
